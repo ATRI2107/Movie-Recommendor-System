c220a732d0da1ae90191144448de743a9b25155d
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-05-22 10:23:19 +0530

    Removed unused modules

--- flaskblog.py
+++ flaskblog.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from datetime import datetime
 from flask_login import LoginManager, UserMixin, login_user 
-import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
